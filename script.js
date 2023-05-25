2a22b440b125627bebde8a6f6298e37305db705a
DARK RO <aaapol007@gmeli.com>
2023-05-25 11:46:16 +0700

    git uplode

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-function myFunction(){
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-    onclick="document.getElementById('demo2').innerHTML = Date()";
+function time() {
+    alert(innerHTML = Date());
 }
